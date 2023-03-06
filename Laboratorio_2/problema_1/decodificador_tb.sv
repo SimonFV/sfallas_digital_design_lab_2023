260e26128be8de67ec52d6731a97ce107ad18caf
Simón Fallas <47420204+SimonFV@users.noreply.github.com>
2023-03-05 21:14:47 -0600

    Mejorado el testbench.

--- Laboratorio_2/problema_1/decodificador_tb.sv
+++ Laboratorio_2/problema_1/decodificador_tb.sv
@@ -8,37 +8,49 @@ module decodificador_tb();
 
   initial
     begin
+		$display("Iniciando simulacion...");
+		
       a = 4'b0000;
-		#40
+		#5;
+		assert(y === 8'b00000000) else $error("0000 fallo");
+		#40;
+		
 		a = 4'b0001;
+		#5;
+		assert(y === 8'b00000001) else $error("0001 fallo");
 		#40;
+		
 		a = 4'b0010;
+		#5;
+		assert(y === 8'b00000010) else $error("0010 fallo");
 		#40;
+		
 		a = 4'b0011;
-		#40
-		a = 4'b0100;
+		#5;
+		assert(y === 8'b00000011) else $error("0011 fallo");
 		#40;
-		a = 4'b0101;
-		#40;
-		a = 4'b0110;
-		#40;
-		a = 4'b0111;
-		#40;
-		a = 4'b1000;
-		#40
-		a = 4'b1001;
-		#40;
-		a = 4'b1010;
-		#40;
-		a = 4'b1011;
-		#40
+		
 		a = 4'b1100;
+		#5;
+		assert(y === 8'b00010010) else $error("1100 fallo");
 		#40;
+		
 		a = 4'b1101;
+		#5;
+		assert(y === 8'b00010011) else $error("1101 fallo");
 		#40;
+		
 		a = 4'b1110;
+		#5;
+		assert(y === 8'b00010100) else $error("1110 fallo");
 		#40;
+		
 		a = 4'b1111;
+		#5;
+		assert(y === 8'b00010101) else $error("1111 fallo");
 		#40;
+		
+		$display("Prueba finalizada.");
+		
     end
 endmodule
