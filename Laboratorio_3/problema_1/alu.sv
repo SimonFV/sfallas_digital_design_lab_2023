3129f5d7ca1b3f40070c7258e29a3575dae8b803
juanpcs <juanpacarrillo1@gmail.com>
2023-03-13 14:44:59 -0600

    Update alu.sv

--- Laboratorio_3/problema_1/alu.sv
+++ Laboratorio_3/problema_1/alu.sv
@@ -2,20 +2,14 @@ module alu #(parameter N = 4)(input logic [N-1:0] a, b,
 										output logic [N-1:0] z, 
 										output logic co);
 
-<<<<<<< Updated upstream
-
 //right_shift_logic #(N) shift_RL (a, b, z);										
 //left_shift_logic #(N) shift_LL (a, b, z);
 //right_shift_arithmetic #(N) shift_RA (a, b, z);
 left_shift_arithmetic #(N) shift_LA (a, b, z);
 //nbit_adder #(N) _adder(a, b, co, z );
 //alu_not #(N) _not(a, z);
+//andGate #(N) _and(a, b, z);
+//orGate #(N) _or(a, b, z);
+//xorGate #(N) _xor(a, b, z);
 
-=======
-//nbit_adder _adder(a, b, co, z );
-//alu_not _not(a, z);
-//andGate _and(a, b, z);
-//orGate _or(a, b, z);
-xorGate _xor(a, b, z);
->>>>>>> Stashed changes
 endmodule
