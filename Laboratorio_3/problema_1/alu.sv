50af898ce87504be1c7df024414f936570991ff4
juanpcs <juanpacarrillo1@gmail.com>
2023-03-13 14:33:30 -0600

    and, or y xor module

--- Laboratorio_3/problema_1/alu.sv
+++ Laboratorio_3/problema_1/alu.sv
@@ -2,6 +2,7 @@ module alu #(parameter N = 4)(input logic [N-1:0] a, b,
 										output logic [N-1:0] z, 
 										output logic co);
 
+<<<<<<< Updated upstream
 
 //right_shift_logic #(N) shift_RL (a, b, z);										
 //left_shift_logic #(N) shift_LL (a, b, z);
@@ -10,4 +11,11 @@ left_shift_arithmetic #(N) shift_LA (a, b, z);
 //nbit_adder #(N) _adder(a, b, co, z );
 //alu_not #(N) _not(a, z);
 
+=======
+//nbit_adder _adder(a, b, co, z );
+//alu_not _not(a, z);
+//andGate _and(a, b, z);
+//orGate _or(a, b, z);
+xorGate _xor(a, b, z);
+>>>>>>> Stashed changes
 endmodule
