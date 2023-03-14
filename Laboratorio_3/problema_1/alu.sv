75785ee264254331424027a52e1306383d815b0c
juanpcs <juanpacarrillo1@gmail.com>
2023-03-13 18:43:40 -0600

    Selector y primeras pruebas de autochequeo

--- Laboratorio_3/problema_1/alu.sv
+++ Laboratorio_3/problema_1/alu.sv
@@ -1,15 +1,43 @@
 module alu #(parameter N = 4)(input logic [N-1:0] a, b, 
+										input logic [3:0] selector,
 										output logic [N-1:0] z, 
 										output logic co);
 
-//right_shift_logic #(N) shift_RL (a, b, z);										
-//left_shift_logic #(N) shift_LL (a, b, z);
-//right_shift_arithmetic #(N) shift_RA (a, b, z);
-left_shift_arithmetic #(N) shift_LA (a, b, z);
-//nbit_adder #(N) _adder(a, b, co, z );
-//alu_not #(N) _not(a, z);
-//andGate #(N) _and(a, b, z);
-//orGate #(N) _or(a, b, z);
-//xorGate #(N) _xor(a, b, z);
+		logic [N-1:0] resultSuma;
+		logic [N-1:0] resultResta;
+		logic [N-1:0] resultNot;
+		logic [N-1:0] resultAnd;
+		logic [N-1:0] resultOr;
+		logic [N-1:0] resultXor;
+		logic [N-1:0] resultSRL;
+		logic [N-1:0] resultSLL;
+		logic [N-1:0] resultSRA;	
+		logic [N-1:0] resultSLA;
+									
+		right_shift_logic #(N) shift_RL (a, b, resultSRL);										
+		left_shift_logic #(N) shift_LL (a, b, resultSLL);
+		right_shift_arithmetic #(N) shift_RA (a, b, resultSRA);
+		left_shift_arithmetic #(N) shift_LA (a, b, resultSLA);
+		nbit_adder #(N) _adder(a, b, co, resultSuma );
+		alu_not #(N) _not(a, resultNot);
+		andGate #(N) _and(a, b, resultAnd);
+		orGate #(N) _or(a, b, resultOr);
+		xorGate #(N) _xor(a, b, resultXor);
+
+always_comb begin
+	case (selector)
+		4'b0000: z = resultSuma;  //0) suma
+		4'b0001: z = resultResta; //1) resta
+		4'b0010: z = resultNot;   //2) not
+		4'b0011: z = resultAnd;   //3) and
+		4'b0100: z = resultOr;    //4) or
+		4'b0101: z = resultXor;   //5) xor
+		4'b0110: z = resultSRL;   //6) shift right lógico
+		4'b0111: z = resultSLL;   //7) shift left lógico
+		4'b1000: z = resultSRA;   //8) shift right aritmético
+		4'b1001: z = resultSLA;   //9) shift left aritmético
+		default: z = 4'b0000;     //operación inválida
+	endcase
+end
 
 endmodule
--- Laboratorio_3/problema_1/alu_tb.sv
+++ Laboratorio_3/problema_1/alu_tb.sv
@@ -3,15 +3,65 @@ module alu_tb();
 parameter N = 4;
 
 logic [N-1:0] a, b, z;
+logic [3:0] selector;
 logic co;
 
-alu #(N) _alu(a, b, z, co);
+alu #(N) _alu(a, b, selector, z, co);
 
 initial begin
 
+//Pruebas and
+
+selector = 4'b0011;
+a = 4'b0001;
+b = 4'b0001;
+#10;
+assert (z === 4'b0001) else $error("and failed.");
+#40;
+
+a = 4'b1001;
+b = 4'b0110;
+#10;
+assert (z === 4'b0000) else $error("and failed.");
+#40;
+
+//Pruebas or
+
+selector = 4'b0100;
+a = 4'b0001;
+b = 4'b0001;
+#10;
+assert (z === 4'b0001) else $error("or failed.");
+#40;
+
+a = 4'b1001;
+b = 4'b0110;
+#10;
+assert (z === 4'b1111) else $error("or failed.");
+#40;
+
+//Pruebas xor
+
+selector = 4'b0101;
+a = 4'b0001;
+b = 4'b0001;
+#10;
+assert (z === 4'b0000) else $error("xor failed.");
+#40;
+
+a = 4'b1001;
+b = 4'b0110;
+#10;
+assert (z === 4'b1111) else $error("xor failed.");
+#40;
+
+//Pruebas shift left arit
+
+selector = 4'b1001;
 a = 4'b0001;
 b = 4'b0001;
 #40;
+
 a = 4'b1011;
 b = 4'b0010;
 #40;
